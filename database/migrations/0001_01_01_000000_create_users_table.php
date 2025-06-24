f6d7e8dbed73975bf249a0a267ab0382e0f0aa0f
mdtomasuddin <mdtomasuddin1@gmail.com>
2025-06-24 21:00:04 +0600

    plan,card_details,Subscription_details migrate create

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -18,9 +18,10 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+            $table->boolean('is_subscribed')->default(0);
             $table->timestamps();
         });
-
+ 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
             $table->string('email')->primary();
             $table->string('token');
---
+++ database/migrations/2025_06_24_132948_create_subscription_plan_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('subscription_plan', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('stripe_price_id')->unique();  
+            $table->integer('trial_days')->nullable();
+            $table->decimal('amount', 10, 2)->nullable(); 
+            $table->integer('type')->comment('0->Monthly, 1->Yearly , 2 -> Lifetime');
+            $table->integer('enabled')->comment('0->disabled , 1->enabled');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('subscription_plan');
+    }
+};
